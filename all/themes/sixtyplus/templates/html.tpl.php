a55f41d5509edc5baf6088ce9b75af4cb751edcc
BLHTECHgit <BLHTECHgit@users.noreply.github.com>
2017-08-22 12:02:22 -0400

    Add files via upload

--- all/themes/sixtyplus/templates/html.tpl.php
+++ all/themes/sixtyplus/templates/html.tpl.php
@@ -986,6 +986,30 @@ jQuery("a[href*='cancer.gov']").attr("target", "_blank");
 
 <script type="text/javascript">_satellite.pageBottom();</script>
 
-  <?php //include("./sites/all/themes/sixtyplus/js/survey-dialog/jquery.confirm.js"); ?>
+
+<!-- Google Code for Remarketing Tag -->
+<!--------------------------------------------------
+Remarketing tags may not be associated with personally identifiable information or placed on pages related to sensitive categories. See more information and instructions on how to setup the tag on: http://google.com/ads/remarketingsetup
+--------------------------------------------------->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 900095112;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/900095112/?guid=ON&amp;script=0"/>
+</div>
+</noscript>
+
+
+<?php //include("./sites/all/themes/sixtyplus/js/survey-dialog/jquery.confirm.js"); ?>
+  
+  
+  
 </body>
 </html>
