45d2eaa3a1926995f3c111c5843a0de9b7bd68c4
BLHTECHgit <BLHTECHgit@users.noreply.github.com>
2020-01-30 10:33:52 -0500

    Add files via upload

---
+++ all/themes/sixtyplus/templates/page--sixtyplus-quit.tpl.php
@@ -0,0 +1,201 @@
+<!--TOP ROW: PART 1 -->
+<div class="container-fluid">
+
+
+        <!--TOP ROW INNER -->
+
+
+
+        <!-- NAV TOP: PART1 -->
+          <?php include("./sites/all/themes/sixtyplus/main_menu.php"); ?>
+        <!-- //NAV TOP: PART1 -->
+
+
+    	<!--//  TOP ROW INNER -->
+
+
+</div>
+<!--//TOP ROW: PART 1-->
+
+
+
+
+<div role="1" id="quit-think" tabindex="-1"></div>
+<!--MIDDLE ROW -->
+<div class="container wrapper-main">
+    <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+      <!--MIDDLE CONTAINER INNER: LEFT TOP -->
+
+      		<div class="row">
+     		 <div class="col-sm-12 col-md-12 col-lg-12 top-items-wrap">
+             <div role="main" id="navskip" tabindex="-1"></div>
+                 <div class="row">
+                     <div class="col-sm-6 col-md-6 col-lg-6">
+                     	<h1 class="hdr-tiles" style="margin-top: 0px;">
+             				<span><a href="/quit/#quit-think">Think.</a> </span>
+                			<span><a href="/quit/#quit-prepare">Prepare.</a></span>
+                			<span><a href="/quit/#quit-quit">Quit.</a> </span><?php //print $title; ?>
+            			</h1>
+                     </div>
+                     <div class="col-sm-6 col-md-6 col-lg-6 pull-right">
+
+                     	 <div class="social-sect" style="margin-top: 10px;"><?php print render($page['social_sect']) ?></div>
+
+                     </div>
+                  </div>
+
+
+
+
+                <?php print render($page['content']) ?>
+              </div>
+            </div>
+      <!--//  MIDDLE CONTAINER INNER: LEFT TOP  -->
+      <!-- MIDDLE CONTAINER INNER: LEFT BOTTOM  -->
+
+      <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+      	<div class="row quit-custom1">
+
+
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+
+                               <h2 class="quit-custom">Think</h2>
+                               <h3 class="quit-custom">What are your<br/>reasons to quit?</h3>
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+
+                           <!-- HOVER EFFECT: BLOCK 2 -->
+                           <div class="bmenu-content">
+                            <ul class="bmenu">
+                                <li><a href="/quit/myself">Quit for myself</a></li>
+                                <li><a href="/quit/health">Quit for my health</a></li>
+                                <li><a href="/quit/finances">Quit for my finances</a></li>
+                                <li><a href="/quit/loved-ones">Quit for my loved ones</a></li>
+                                <li><a href="/quit/not-ready">Not yet ready to quit</a></li>
+                                <li><a href="/quit/tobacco-101">Tobacco 101</a></li>
+                            </ul>
+                                     <div role="2" id="quit-prepare" tabindex="-1"></div>
+                           </div>
+                        	<!-- //HOVER EFFECT: BLOCK 2 -->
+
+                    </div>
+
+        </div>
+
+
+
+      </div>
+      </div>
+       <!-- //MIDDLE CONTAINER INNER: LEFT BOTTOM  -->
+
+
+
+             <!-- MIDDLE CONTAINER INNER: LEFT BOTTOM 2 -->
+      <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+      	<div class="row quit-custom2">
+
+
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+
+                               <!-- HOVER EFFECT: BLOCK 1 -->
+                               <h2 class="quit-custom">Prepare</h2>
+                               <h3 class="quit-custom">Prepare for<br/>your quit date</h3>
+                        	<!-- //HOVER EFFECT: BLOCK 1 -->
+
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+
+                           <!-- HOVER EFFECT: BLOCK 2 -->
+                           <div class="bmenu-content" >
+                            <ul class="bmenu">
+                                <li><a href="/quit/withdrawal">Dealing with withdrawal</a></li>
+                                <li><a href="/quit/tools">Quitting tools and medications</a></li>
+                                <li><a href="/quit/slip">Handling a slip</a></li>
+
+                            </ul>
+                           </div>
+                        	<!-- //HOVER EFFECT: BLOCK 2 -->
+                                   <div role="3" id="quit-quit" tabindex="-1"></div>
+                    </div>
+
+        </div>
+
+
+
+      </div>
+      </div>
+       <!-- //MIDDLE CONTAINER INNER: LEFT BOTTOM 2  -->
+
+
+
+                    <!-- MIDDLE CONTAINER INNER: LEFT BOTTOM 3 -->
+      <div class="row">
+      <div class="col-sm-12 col-md-12 col-lg-12">
+      	<div class="row quit-custom3">
+
+
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+
+                               <!-- HOVER EFFECT: BLOCK 1 -->
+                               <h2 class="quit-custom">Quit</h2>
+                               <h3 class="quit-custom">Pledge to quit<br/>and create your<br/>quit plan</h3>
+
+                        	<!-- //HOVER EFFECT: BLOCK 1 -->
+
+                    </div>
+                    <div class="col-sm-6 col-md-6 col-lg-6">
+
+                           <!-- HOVER EFFECT: BLOCK 2 -->
+                           <div class="bmenu-content">
+                            <ul class="bmenu">
+                                <li><a href="/quitplan">Start your quit plan online today</a></li>
+                                <li><a href="/sites/default/files/pdfs/smokefree_60plus_quit_plan.pdf" target="_blank">Print a copy of the quit plan</a></li>
+                                <li><a href="/quit/help-a-loved-one-quit">Help a loved one quit</a></li>
+                            </ul>
+                           </div>
+                        	<!-- //HOVER EFFECT: BLOCK 2 -->
+                    </div>
+
+        </div>
+
+
+
+      </div>
+      </div>
+       <!-- //MIDDLE CONTAINER INNER: LEFT BOTTOM 3  -->
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+</div>
+<!--//  MIDDLE ROW -->
+
+
+
+
+<!--BOTTOM ROW: FOOTER SECTION-->
+	 <?php include("./sites/all/themes/sixtyplus/templates/footer.php"); ?>
+
+<!--//  BOTTOM ROW: FOOTER SECTION -->
+<!-- INTERNAL PAGE SCRIPTS -->
+ <?php include("./sites/all/themes/sixtyplus/js/text-size-script/text-size-inc-dec.js"); ?>
+<?php include("./sites/all/themes/sixtyplus/js/jquery-skip-to-3.js"); ?>
+
+
+<!-- //INTERNAL PAGE SCRIPTS -->
