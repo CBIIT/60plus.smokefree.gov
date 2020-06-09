3d76c601411f70a50d69bc2463758b1781e269b0
BLHTECHgit <BLHTECHgit@users.noreply.github.com>
2020-06-09 08:16:12 -0400

    Add files via upload

---
+++ all/themes/sixtyplus/templates/page--front.tpl.php
@@ -0,0 +1,222 @@
+<!--TOP ROW: PART 1 -->
+
+<div class="container-fluid">
+
+  <!--TOP ROW INNER -->
+
+  <!-- NAV TOP: PART1 -->
+  <?php include("./sites/all/themes/sixtyplus/main_menu.php"); ?>
+  <!-- //NAV TOP: PART1 -->
+
+  <!--//  TOP ROW INNER -->
+
+</div>
+<!--//TOP ROW: PART 1-->
+
+<!--MIDDLE ROW -->
+<div role="main" id="navskip" tabindex="-1"></div>
+<div class="container wrapper-main">
+  <div class="row">
+    <div class="col-sm-8 col-md-8 col-lg-8">
+
+      <!-- MIDDLE CONTAINER INNER: LEFT BOTTOM  -->
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="row">
+            <div class="col-sm-4 col-md-4 col-lg-4 responsive768">
+
+              <!-- HOVER EFFECT: BLOCK 1 -->
+              <div class="grid">
+                <figure class="effect-apollo hover-front"> <img src="<?php print $directory; ?>/images/125x125_TPQ2.jpg" alt="Think"/>
+                  <!--<div style="background-color:#7d868d" class="effect-block"></div>-->
+                  <!--<img src="<?php //print $directory; ?>/images/50plus_front_I_want_to_quit_608x768.jpg" alt="Image for I want to Quit"/>  -->
+                  <figcaption>
+                    <!-- <h2>I want to <span>Quit!</span></h2>-->
+                    <!--<h2><span>Think.</span></h2>-->
+                    <p>What are your reasons for wanting to quit</p>
+                    <a href="../quit/#quit-think">View the Think section.</a>
+                    <!--<a href="../quit/">View the "Quit for Myself." page.</a>-->
+                  </figcaption>
+                </figure>
+              </div>
+              <!-- //HOVER EFFECT: BLOCK 1 -->
+
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 responsive768">
+
+              <!-- HOVER EFFECT: BLOCK 2 -->
+              <div class="grid">
+                <figure class="effect-apollo hover-front"> <img src="<?php print $directory; ?>/images/prepare_480.jpg" alt="Prepare"/>
+
+                  <!--<div style="background-color:#420098" class="effect-block"></div>-->
+                  <!--<img src="<?php //print $directory; ?>/images/50plus_front_help_loved_one_989x768.jpg" alt="Not yet ready to Quit."/>-->
+                  <figcaption>
+                    <!--<h2><span>Prepare.</span></h2>-->
+                    <!--<h2>Not yet ready to <span>Quit</span></h2>-->
+                    <p>Prepare for your quit date</p>
+                    <a href="../quit/#quit-prepare">View the Prepare section.</a> </figcaption>
+                </figure>
+              </div>
+              <!-- //HOVER EFFECT: BLOCK 2 -->
+
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 responsive768">
+              <!-- HOVER EFFECT: BLOCK 3 -->
+              <div class="grid">
+                <figure class="effect-apollo hover-front"> <img src="<?php print $directory; ?>/images/125x125_TPQ23.jpg" alt="Quit"/>
+                  <!--<div style="background-color:#3f6f33" class="effect-block"></div>-->
+                  <!--<img src="<?php //print $directory; ?>/images/50plus_front_help_loved_one_796x768.jpg" alt="Help a loved one Quit."/>-->
+                  <figcaption>
+                    <!--<h2><span>Quit.</span></h2>-->
+                    <!--<h2>Help a loved one <span>Quit</span></h2>-->
+                    <p>Pledge to quit and create your plan now</p>
+                    <a href="../quit/#quit-quit">View the Quit section.</a> </figcaption>
+                </figure>
+              </div>
+              <!-- //HOVER EFFECT: BLOCK 3 -->
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- //MIDDLE CONTAINER INNER: LEFT BOTTOM  -->
+      <!--MIDDLE CONTAINER INNER: LEFT TOP -->
+
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12 top-items-wrap">
+          <!--START: bootstrap carousel -->
+          <div id="carousel-example-generic" class="carousel slide homeCarousel" data-ride="carousel" data-interval="8000">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+              <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <!-- Slide 1 -->
+              <div class="item active" id="carouselOne"><a href="/support/friends-and-family"> <img src="<?php print $directory; ?>/images/carousel/sf-60plus-carousel-meet-other-quitters.jpg" alt="A group of friends smiling for the camera."></a>
+                <div class="carousel-caption">
+                  <h3>Tell your friends and family your plans to quit. <a class="carousel-caption-link" href="/support/friends-and-family">Learn how they can help.></a></h3>
+                </div>
+              </div>
+              <!-- END: Slide 1 -->
+			  <!-- Slide 2 -->
+              <div class="item" id="cauroselTwo"><a href="/quit/health"> <img src="<?php print $directory; ?>/images/carousel/sf-60plus-carousel-banff.jpg" alt="A lake and mountains."></a>
+                <div class="carousel-caption">
+                  <h3>Breathe healthily, live happily. <a class="carousel-caption-link" href="/quit/health">Quit for your health.></a></h3>
+                </div>
+              </div>
+              <!-- END: Slide 2 -->
+              <!-- Slide 3 -->
+              <div class="item" id="carouselThree"><a href="/support/quitline"> <img src="<?php print $directory; ?>/images/carousel/sf-60plus-carousel-call-number.jpg" alt="Call 1-800-44U-QUIT"></a>
+                <div class="carousel-caption">
+                  <h3>Need a little extra support?  <a class="carousel-caption-link" href="/support/quitline">Call a quit line.></a></h3>
+                </div>
+              </div>
+              <!-- END: Slide 3 -->
+              <!-- Slide 4 -->
+              <div class="item" id="carouselFour"><a href="/quit/withdrawal"> <img src="<?php print $directory; ?>/images/carousel/sf-60plus-carousel-onestep.jpg" alt="Woman walking on a beach at sunset."></a>
+                <div class="carousel-caption" style="bottom:50px;">
+                  <h3>Quitting might sound overwhelming. Focus on one step at a time. <a class="carousel-caption-link" href="/quit/withdrawal">Click here to learn more.></a></h3>
+                </div>
+              </div>
+              <!-- END: Slide 4 -->
+            </div>
+            <!-- //Wrapper for slides -->
+            <!-- Controls -->
+            <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            	<span class="fa fa-chevron-left fa-3x fa-position" aria-hidden="true"></span>
+            	<span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            	<span class="fa fa-chevron-right fa-3x fa-position" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+            <div id="carouselButtons" style="margin-left: 85%; position: absolute; bottom: 10px;">
+            	<a href="#play" id="playButton" role="button" class="btn btn-default btn-xs playSpace" >
+                	<!--<span title="play button" class="fa fa-play fa-3x" aria-hidden="true"></span>//-->
+                    <img class="playImage" src="<?php print $directory; ?>/css/image-icons/32/play.png" alt="play" aria-hidden="true"/>
+                    <span class="sr-only">play</span>
+                </a>
+                <a href="#pause" id="pauseButton" role="button" class="btn btn-default btn-xs pauseSpace">
+                	<!--<span title="pause button" class="fa fa-pause fa-3x" aria-hidden="true"></span>//-->
+                    <img class="pauseImage" src="<?php print $directory; ?>/css/image-icons/32/pause.png" alt="pause" aria-hidden="true"/>
+                    <span class="sr-only">pause</span>
+                </a>
+            </div>
+
+         </div>
+
+          <!--END: bootstrap carousel -->
+        </div>
+      </div>
+
+      <!--//  MIDDLE CONTAINER INNER: LEFT TOP  -->
+
+    </div>
+    <div class="col-sm-4 col-md-4 col-lg-4 responsive768">
+      <!--MIDDLE CONTAINER INNER: RIGHT -->
+
+      <!--MIDDLE CONTAINER INNER: RIGHT - ROW 1 -->
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="MIDDLE-CONTAINER-INNER-RIGHT-ROW1">
+            <div class="panel panel-default">
+              <div class="panel-body sixtyplus-intro">
+                <h1 class="top-items-title-1">A message about COVID-19 for older adults who smoke.</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--// MIDDLE CONTAINER INNER: RIGHT - ROW 1 -->
+
+      <!--MIDDLE CONTAINER INNER: RIGHT - ROW 2 -->
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="MIDDLE-CONTAINER-INNER-RIGHT-ROW2"> <?php print render($page['sixtyplus_front_right_row2']) ?> </div>
+        </div>
+      </div>
+
+      <!--// MIDDLE CONTAINER INNER: RIGHT - ROW 2 -->
+
+      <!--MIDDLE CONTAINER INNER: RIGHT - ROW 3 -->
+      <div class="row">
+        <div class="col-sm-12 col-md-12 col-lg-12">
+          <div class="MIDDLE-CONTAINER-INNER-RIGHT-ROW3">
+            <div class="panel panel-default">
+              <div class="panel-body quit-plan"> <a style="text-decoration:none; color:#333;"  href="../quitplan">
+                <!--<img class="img-responsive pull-left" src="/sites/all/themes/sixtyplus/images/calendar_black.png" alt="Image icon of a calendar representing the quit plan.">-->
+
+                <i class="fa flaticon-calendar146 fa-4x pull-left" aria-hidden="true"></i>
+                <h2 class="top-items-title-2" >Pledge To Quit and Plan How Today!</h2>
+                </a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!--// MIDDLE CONTAINER INNER: RIGHT - ROW 3 -->
+
+      <!--//  MIDDLE CONTAINER INNER: RIGHT -->
+    </div>
+  </div>
+</div>
+<!--//  MIDDLE ROW -->
+
+<!--BOTTOM ROW: FOOTER SECTION-->
+<?php include("./sites/all/themes/sixtyplus/templates/footer.php"); ?>
+
+<!--//  BOTTOM ROW: FOOTER SECTION -->
+<!-- INTERNAL PAGE SCRIPTS -->
+
+<?php include("./sites/all/themes/sixtyplus/js/bootsrap-carousel-mods.js"); ?>
+<?php include("./sites/all/themes/sixtyplus/js/menu-active/menu-active.js"); ?>
+<?php include("./sites/all/themes/sixtyplus/js/text-size-script/text-size-inc-dec.js"); ?>
+<?php //include("./sites/all/themes/sixtyplus/js/jquery-skip-to-3.js"); ?>
+
+<!-- //INTERNAL PAGE SCRIPTS -->
